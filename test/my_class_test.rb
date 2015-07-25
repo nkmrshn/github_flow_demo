4b4443756ae7185cb1c91adf90783a95851633d8
nkmrshn <nkmrshn@gmail.com>
2015-07-25 17:41:16 +0900

    add even test

--- test/my_class_test.rb
+++ test/my_class_test.rb
@@ -11,6 +11,7 @@ class MyClassTest < Minitest::Test
   end
 
   def test_even?
-    skip "test later"
+    assert_equal false, @my_class.even?(1)
+    assert_equal true,  @my_class.even?(2)
   end
 end
